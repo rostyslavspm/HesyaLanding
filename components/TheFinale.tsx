4244f901a8079f97039a4022e0dd616b6a429944
rostyslavspm <rostyslav.slobodianiuk@gmail.com>
2026-03-24 19:42:14 +0100

    chore(ui): remove system operational component

--- components/TheFinale.tsx
+++ components/TheFinale.tsx
@@ -48,13 +48,7 @@ export default function TheFinale() {
 
       </div>
 
-      <div className="absolute bottom-8 right-8 flex items-center gap-3 finale-content">
-        <span className="text-xs font-sans text-[var(--color-soft-obsidian)]/50 tracking-wider uppercase">System Operational</span>
-        <div className="relative flex items-center justify-center">
-          <div className="absolute w-5 h-5 bg-green-500/20 rounded-full animate-[ping_2.5s_ease-out_infinite]" />
-          <div className="relative w-2.5 h-2.5 bg-green-500 rounded-full shadow-[0_0_8px_rgba(34,197,94,0.6)]" />
-        </div>
-      </div>
+
     </footer>
   );
 }
